0b2aa8a2c207496e6b7375247c9cca29dc472bfb
Fazi_Cydeo <fazi.6246@mail.ru>
2023-03-09 18:16:18 -0500

    changed smoke

--- src/test/java/com/library/step_definitions/LoginStepDefs.java
+++ src/test/java/com/library/step_definitions/LoginStepDefs.java
@@ -10,8 +10,7 @@ public class LoginStepDefs {
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-       loginPage.login(ConfigurationReader.getProperty("librarian_username"),
-                ConfigurationReader.getProperty("librarian_password"));
+
     }
 
     @Given("the user logged in as {string}")
